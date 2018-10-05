4ac1f28efcedaaa55014751170a308a44f463f4b
YourtionGuo <yourtion@gmail.com>
2018-10-05 14:54:12 +0800

    feat(message): 添加 thrift 定义

---
+++ thrift/message/gen-code.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+thrift --gen py -out ../message-service message.thrift
+
+thrift --gen java -out ../../message-thrift-api/src/main/java/ message.thrift
---
+++ thrift/message/message.thrift
@@ -0,0 +1,7 @@
+namespace java com.yourtion.micro.thrift.message
+namespace py message.api
+
+service MessageService {
+  bool sendMobileMessage(1: string mobile, 2: string message);
+  bool sendEmailMessage(1: string email, 2: string message);
+}
