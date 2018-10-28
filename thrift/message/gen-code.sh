1e8d20a193cfd93e1125b6417ae9c974ebd40f5f
YourtionGuo <yourtion@gmail.com>
2018-10-28 15:27:09 +0800

    refactor(service): 重命名 service 模块，添加 thrift

--- thrift/message/gen-code.sh
+++ thrift/message/gen-code.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-thrift --gen py -out ../../message-service message.thrift
+thrift --gen py -out ../../message-thrift-service message.thrift
 
 thrift --gen java -out ../../message-thrift-api/src/main/java/ message.thrift
